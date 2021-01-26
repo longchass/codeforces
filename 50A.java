66a3af83388af508707484137734f9945c0b6918
longchass <longchass@yahoo.com>
2021-01-26 20:29:25 +1100

    Update 50A.java

--- 50A.java
+++ 50A.java
@@ -1,29 +1,13 @@
 import java.util.*;
 import java.io.*;
-
+ 
 public class Main{
 	public static void main(String[] args) throws Exception{
 		Scanner input = new Scanner(System.in);
-		int loop = input.nextInt();
-		int res =0;
-		 input.nextLine();
-		for(int i=0; i < loop;i++)
-		{
-			String line = input.nextLine();
-			char in = line.charAt(0);
-			if(in == 'X')
-			{
-				in = line.charAt(1);
-			}
-			if (in ==  '-')
-			{
-				res--;
-			}
-			else
-			{
-				res++;
-			}
-		}
+		int x = input.nextInt();
+		int y = input.nextInt();
+ 
+		int res = x*y/2;
 		System.out.println(res);
 	}
 }
